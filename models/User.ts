bb1e135d5b57d5ff12079c9b19333fdfe78148a6
Gordus Alexander <aldrmiller@gmail.com>
2024-09-08 17:09:08 +0600

    реализовал сущность User, типизировал данные.

---
+++ models/User.ts
@@ -0,0 +1,50 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import {UserFields, UserMethods, UserModel} from '../types';
+import {randomUUID} from 'node:crypto';
+
+const SALT_WORK_FACTOR = 10;
+
+const UserSchema = new mongoose.Schema<UserFields, UserModel, UserMethods>({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  token: {
+    type: String,
+    required: true,
+  }
+});
+
+UserSchema.methods.checkPassword = function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+UserSchema.methods.generateToken = function () {
+  this.token = randomUUID();
+};
+
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+  this.password = await bcrypt.hash(this.password, salt);
+
+  next();
+});
+
+UserSchema.set('toJSON', {
+  transform: (_doc, ret) => {
+    delete ret.password;
+    return ret;
+  }
+});
+
+const User = mongoose.model<UserFields, UserModel>('User', UserSchema);
+export default User;

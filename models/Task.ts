09b7726ce3442344591931ab8af0605ddb12d0d0
Gordus Alexander <aldrmiller@gmail.com>
2024-09-08 18:18:56 +0600

    реализовал сущность Task, типизировал данные.

---
+++ models/Task.ts
@@ -0,0 +1,29 @@
+import mongoose, {Schema, Types} from 'mongoose';
+import User from './User';
+import {TaskFields} from '../types';
+
+const TaskSchema = new mongoose.Schema<TaskFields>({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+    validate: {
+      validator: async (value: Types.ObjectId) => {
+        const user = await User.findById(value);
+        return Boolean(user);
+      },
+      message: 'User does not exist',
+    }
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  status: {
+    enum: ['new', 'in_progress', 'complete'],
+    default: 'new',
+  }
+});
+const Task = mongoose.model('Task', TaskSchema);
+export default Task;
--- types.ts
+++ types.ts
@@ -1,9 +1,19 @@
-import {Model} from 'mongoose';
+import {Model, Schema} from 'mongoose';
 
 export interface UserFields {
   username: string;
   password: string;
   token: string;
+}
+
+export interface TaskFields {
+  user: Schema.Types.ObjectId;
+  title: string;
+  description: string;
+  status: {
+    enum: Array<string>;
+    default: string;
+  };
 }
 
 export interface UserMethods {
